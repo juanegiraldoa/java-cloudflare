4105bddb1b5e38d384c9705c0b67a2e34e3c027d
Juan Giraldo <juanegiraldoa@proton.me>
2025-07-27 01:39:35 -0500

    Add hono

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import { Container, getContainer } from '@cloudflare/containers';
+import { Hono } from 'hono';
 
 export class JavaContainer extends Container {
 	defaultPort = 8080;
 	sleepAfter = '2m';
 }
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		const container = getContainer(env.CONTAINER);
-		return await container.fetch(request);
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono<{
+	Bindings: { CONTAINER: DurableObjectNamespace<JavaContainer> };
+}>();
+
+app.get('/', async (c) => {
+	const container = getContainer(c.env.CONTAINER);
+	return await container.fetch(c.req.raw);
+});
+
+export default app;
